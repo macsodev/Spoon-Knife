cd014ec5734e7bfe9f4a98b33640a2120f4750cf
macsodev <macsodev@gmail.com>
2016-03-18 08:11:12 +0100

    Hello World project with C# basic syntax examples.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello\n\nWorld!");
+            Console.WriteLine("Integer: " + 10 + " Double: " + 3.14 + " Boolean: " + true);
+
+            Console.Write("Kiiras új sor nélkül :)");
+
+            // Decimal - a 128 - bits data type, with more precision than other floating - point types,
+            // suited for financial and monetary calculations
+            decimal fooDecimal = 150.3m;
+
+            // Boolean - true & false
+            bool fooBoolean = true; // or false
+
+            // Char - A single 16-bit Unicode character
+            char fooChar = 'A';
+
+            // Strings -- unlike the previous base types which are all value types,
+            // a string is a reference type. That is, you can set it to null
+            string fooString = "\"escape\" quotes and add \n (new lines) and \t (tabs)";
+            Console.WriteLine(fooString);
+
+            // Formatting, based on sprintf
+            string fooFs = string.Format("Check Check, {0} {1}, {0} {1:0.0}", 1, 2);
+            Console.WriteLine(fooFs);
+
+        }
+    }
+}
